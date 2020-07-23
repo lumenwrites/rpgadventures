42a47f9b2fe8b48052d97ceb32279014b05072a2
Ray Alez <raymestalez@gmail.com>
2020-07-23 03:34:44 +0300

    Remove prismic and smartypants.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+//import "typeface-montserrat"
+//import "typeface-merriweather"
 
-import "prismjs/themes/prism.css"
+//import "prismjs/themes/prism.css"
 
 import wrapWithProvider from "./wrap-with-provider"
 export const wrapRootElement = wrapWithProvider
