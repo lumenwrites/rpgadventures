1892f7b2d05f1276b5cbf0ffda679fbea5bc6ede
Ray Alez <raymestalez@gmail.com>
2021-03-02 07:58:03 +0000

    Update

--- src/pages/template.js
+++ src/pages/template.js
@@ -6,13 +6,13 @@ import SEO from "../components/seo"
 
 const TemplateRedirect = () => {
   if (typeof window !== "undefined") {
-    window.location.replace("https://docs.google.com/document/d/1R7bOixB-1tN7U3lEi47L-NDA91CibDJ0VTgrbKO2nlQ/")
+    window.location.replace("https://docs.google.com/document/d/1Eg6jXg47vivpyKBfPQnO_SKSV0BIO1RMurGhRSc1rDA/")
   }
 
   return (
     <Layout>
 	<article>
-	    <a href="https://docs.google.com/document/d/1R7bOixB-1tN7U3lEi47L-NDA91CibDJ0VTgrbKO2nlQ/">Adventure Template</a>
+	    <a href="https://docs.google.com/document/d/1Eg6jXg47vivpyKBfPQnO_SKSV0BIO1RMurGhRSc1rDA/">Story Template</a>
 	</article>
     </Layout>
   )
