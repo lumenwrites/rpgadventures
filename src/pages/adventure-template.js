e1561f3104efe67b1789d9fbd5097dfe865b709d
Ray Alez <raymestalez@gmail.com>
2021-05-18 17:07:02 +0100

    Update.

--- src/pages/adventure-template.js
+++ src/pages/adventure-template.js
@@ -6,13 +6,13 @@ import SEO from "../components/seo"
 
 const TemplateRedirect = () => {
   if (typeof window !== "undefined") {
-    window.location.replace("https://docs.google.com/document/d/15s0bm3G3AnOVxayBSZ8X1RbazSWdB-HXPSFTTCQVWxY/")
+    window.location.replace("https://docs.google.com/document/d/1yUqvSokoxJalKhi236xvl4q1cf7EWLVmrfgw9ljGyMI/")
   }
 
   return (
     <Layout>
       <article>
-        <a href="https://docs.google.com/document/d/15s0bm3G3AnOVxayBSZ8X1RbazSWdB-HXPSFTTCQVWxY/">Adventure Template</a>
+        <a href="https://docs.google.com/document/d/1yUqvSokoxJalKhi236xvl4q1cf7EWLVmrfgw9ljGyMI/">Adventure Template</a>
       </article>
     </Layout>
   )
